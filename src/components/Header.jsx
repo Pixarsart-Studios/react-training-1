d0a98cc606081aac1e1b9a344478344b63a450f7
Hamza Khalid <hamza@pixarsart.com>
2023-07-13 13:35:42 +0500

    feat(setup): project setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MainNav from "./MainNav";
+import TopNav from "./TopNav";
+
+const Header = () => {
+  return (
+    <div>
+      <TopNav />
+      <MainNav />
+    </div>
+  );
+};
+
+export default Header;
